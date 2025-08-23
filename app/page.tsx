4bd8acb6d4bd1fab87d5bb2f87e0e8f8cb902536
pictomo <tomo.pict@gmail.com>
2025-08-23 23:48:40 +0900

    [feat]default content (ja locale content) set when js is deactivated

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Home from "./components/home";
+import { defaultLocale } from "./i18n/config";
 
 const Page = () => {
   const router = useRouter();
@@ -10,7 +12,11 @@ const Page = () => {
     router.replace("/ja");
   }, []);
 
-  return null;
+  return (
+    <noscript>
+      <Home locale={defaultLocale} />
+    </noscript>
+  );
 };
 
 export default Page;
