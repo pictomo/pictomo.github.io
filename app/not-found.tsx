30800aca05e4856347fc06bf340d72218afd36b0
pictomo <tomo.pict@gmail.com>
2025-08-25 00:02:12 +0900

    [feat]style not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,38 +4,41 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FaRegSmileBeam } from "react-icons/fa";
 import LinkStyle from "@/app/styles/link.module.scss";
+import CenteredStyle from "@/app/styles/centered.module.scss";
 
 const NotFound = () => {
   const router = useRouter();
 
   return (
-    <div>
-      <h1>404 | Not Found</h1>
-      <p>
-        Return to{" "}
-        <a className={LinkStyle.link} onClick={() => router.back()}>
-          previous page
-        </a>
-        .
-      </p>
-      <p>
-        Go to{" "}
-        <Link className={LinkStyle.link} href="/">
-          top page
-        </Link>
-        .
-      </p>
-      <p>
-        As an aside, I really love this music{" "}
-        <a
-          className={LinkStyle.link}
-          href="https://youtu.be/gnnIrTLlLyA?si=JRpU6ue7rnpy9Xsd"
-          target="_blank"
-        >
-          Knife Party - 404
-        </a>{" "}
-        <FaRegSmileBeam />
-      </p>
+    <div className={CenteredStyle.viewPort}>
+      <div>
+        <h1>404 | Not Found</h1>
+        <p>
+          Return to{" "}
+          <a className={LinkStyle.link} onClick={() => router.back()}>
+            previous page
+          </a>
+          .
+        </p>
+        <p>
+          Go to{" "}
+          <Link className={LinkStyle.link} href="/">
+            top page
+          </Link>
+          .
+        </p>
+        <p>
+          As an aside, I really love this music{" "}
+          <a
+            className={LinkStyle.link}
+            href="https://youtu.be/gnnIrTLlLyA?si=JRpU6ue7rnpy9Xsd"
+            target="_blank"
+          >
+            Knife Party - 404
+          </a>{" "}
+          <FaRegSmileBeam />
+        </p>
+      </div>
     </div>
   );
 };
