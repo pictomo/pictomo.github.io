f8429460b2f7799eb6a54a12c98d1d9f06e6d3ee
pictomo <tomo.pict@gmail.com>
2025-08-25 22:21:59 +0900

    [feat]update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,23 @@ export const metadata: Metadata = {
   title: "pictomo",
   description: `
   pictomo / ピクトモ / ぴくとも
-  pictomoの自己紹介、ポートフォリオサイトです。ITエンジニアの卵をやっております。
-  This is pictomo's self-introduction and portfolio site. I'm an aspiring IT engineer.
+  自己紹介、ポートフォリオサイトです。ITエンジニアの卵をやっております。
+  This is self-introduction and portfolio site. I'm an aspiring IT engineer.
   `,
+  keywords: [
+    "pictomo",
+    "ピクトモ",
+    "ぴくとも",
+    "self introduction",
+    "自己紹介",
+    "portfolio",
+    "ポートフォリオ",
+    "IT engineer",
+    "ITエンジニア",
+    "Japan",
+    "日本",
+  ],
+  icons: "/favicon.ico",
   verification: {
     google: "UcpS98FWCatzamy3CSYV17WDqJdIusn5PK0j9mp0qbc",
   },
