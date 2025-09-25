72384ab0f1cd5d05c4e876677ac25339cc197007
pictomo <tomo.pict@gmail.com>
2025-09-25 23:33:54 +0900

    [fix]resolve hydration error #6

--- app/components/themeChanger.tsx
+++ app/components/themeChanger.tsx
@@ -3,20 +3,35 @@
 import { useTheme } from "next-themes";
 import { I18nLocale, t } from "@/app/i18n/config";
 import linkStyles from "@/app/styles/link.module.scss";
+import { useEffect, useState } from "react";
 
 export const ThemeChanger = ({ locale }: { locale: I18nLocale }) => {
   const { theme, setTheme } = useTheme();
+
+  // 直接themeを参照するとbuildされたものと食い違いHydration Errorとなる
+  // themeAfterHydrationをクッションとして用いる
+  const [themeAfterHydration, setThemeAfterHydration] = useState<
+    string | undefined
+  >();
+
+  useEffect(() => {
+    setThemeAfterHydration(theme);
+  }, []);
 
   return (
     <div>
       <span
         className={linkStyles.link}
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        onClick={() => {
+          setTheme(theme === "light" ? "dark" : "light");
+          setThemeAfterHydration(theme === "light" ? "dark" : "light");
+        }}
       >
         {t(
           {
-            ja: theme === "dark" ? "ライトモード" : "ダークモード",
-            en: theme === "dark" ? "Light Mode" : "Dark Mode",
+            ja:
+              themeAfterHydration === "dark" ? "ライトモード" : "ダークモード",
+            en: themeAfterHydration === "dark" ? "Light Mode" : "Dark Mode",
           },
           locale
         )}
