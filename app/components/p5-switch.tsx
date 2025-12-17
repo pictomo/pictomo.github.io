fe359ac06baf2e7ffb0345424c224b8403306ea0
pictomo <tomo.pict@gmail.com>
2025-12-17 15:51:57 +0900

    feat: change comment about background

--- app/components/p5-switch.tsx
+++ app/components/p5-switch.tsx
@@ -35,8 +35,8 @@ const P5Switch = ({ locale }: { locale: I18nLocale }) => {
         <span className={tooltipStyles.text}>
           {t(
             {
-              ja: "試験運用中の機能\n不具合のある場合はoffにしてください",
-              en: "Experimental feature\nPlease turn it off if there are any issues",
+              ja: "パフォーマンスが悪い場合はoffにしてください",
+              en: "Please turn it off if it performs poorly",
             },
             locale
           )}
